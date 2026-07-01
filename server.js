34d69ffe065f883e7274ca6c89a0cb21ef0c46c1
devjohnnydev <johnnybraga2@gmail.com>
2026-07-01 16:11:39 -0300

    fix(dns): set default result order to ipv4first to solve ENETUNREACH in environments without IPv6 routing

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const dns = require('dns');
+if (dns.setDefaultResultOrder) {
+    dns.setDefaultResultOrder('ipv4first');
+}
+
 const express = require('express');
 const dotenv  = require('dotenv');
 const path    = require('path');
